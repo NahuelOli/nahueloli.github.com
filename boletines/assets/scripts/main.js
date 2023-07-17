679a6d0151cd236e4de8702b611078ee4d2536ce
NahueOli <nahuolivera55@gmail.com>
2023-07-16 21:30:09 -0300

    Curso de desarrollo web terminado

--- boletines/assets/scripts/main.js
+++ boletines/assets/scripts/main.js
@@ -1,35 +1,29 @@
-// DOM
-// querySelector - querySelectorAll
-//
-// let links = document.querySelectorAll(".info");
-// links.forEach(function(link){
-//     console.log(link);
-// });
 
-// let celdas = document.querySelectorAll("td");
-
-// celdas.forEach(function (td) {
-//   td.addEventListener("click", function () {
-//     console.log(this);
-//   })
-// });
-
-//Obtener los elementos de la clase .close
+// Obtener los elementos de la clase .close
 let links = document.querySelectorAll(".close");
 
-//Recorrerlos
+// Recorrerlos
 links.forEach(function (link) {
-  //Agregar un evento click a cada uno de ellos
-  link.addEventListener("click", function(ev) {
+  //   Agregar un evento click a cada uno de ellos
+  link.addEventListener("click", function (ev) {
     ev.preventDefault();
+    let content = document.querySelector(".content");
+
+    // Quitarle las clases de animacion que ya tiene
+    content.classList.remove("fadeInDown");
+    content.classList.remove("animated");
+
+    // Agregar clases para animar su salida fadeOutUp
+    content.classList.add("fadeOutUp");
+    content.classList.add("fadeInDown");
+
+    // timers
+    // setInterval
+    setTimeout(function () {
+      // Setea ruta
+      location.href = "/boletines";
+    }, 600);
+
+    return false;
   });
-});
-
-
-
-let iconos = document.querySelectorAll("i");
-
-iconos.forEach(function(icono){
-    icono.classList.remove("fa-star-o");
-    icono.classList.add("fa-star");
 });
